10bfc632d0472063254ee46468d45198326ff2ad
Saugat Bhattarai <the.saggitorres@gmail.com>
2020-02-27 17:33:50 +0545

    to dispatch actions based on the props send to the components

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./App.css";
 import CakeContainer from "./components/CakeContainer";
-import HooksCakeContainer from "./components/HooksCakeContainer";
+// import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
 import ChocolateContainer from "./components/ChocolateContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
+import ItemContainer from "./components/ItemContainer";
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         {/* <HooksCakeContainer /> */}
         <h1>Stocks in My Store</h1>
+        <hr />
+        <h2>To dispatch actions based on the props send to component</h2>
+        <div>====================</div>
+        <ItemContainer cake />
+        <ItemContainer />
         <hr />
         <CakeContainer />
         <IceCreamContainer />
